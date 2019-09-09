858d698d4ac5aab826486d445a59756472cb468c
vagrant <vagrant@vagrant.vm>
2019-09-09 13:08:22 +0000

    .

--- brainfuck.cpp
+++ brainfuck.cpp
@@ -16,11 +16,12 @@ struct ProgramSeq {
 
 struct VMachine {
   int pointer;
+  ProgramSeq seq;
   vector<int> memory;
 };
 
 void brainfuck_execute(string code, VMachine *machine);
-void sequence_execute(char instruction, VMachine *machine);                                            
+void sequence_execute(char instruction, VMachine *machine, int pc);                                            
 
 
 ProgramSeq parse_from_stream(istream& streams) {
@@ -56,31 +57,34 @@ ProgramSeq parse_from_stream(istream& streams) {
 
 void brainfuck_execute(string code, VMachine *machine) {
 
+  int pc = 0;
+
   for(char i : code) {
 
-    sequence_execute(i, machine);
+    pc++;
+    sequence_execute(i, machine, pc);
 
   }
 
 }
 
 
-void sequence_execute(char instruction, VMachine *machine){
+void sequence_execute(char instruction, VMachine *machine, int pc){
 
   int addres = machine->pointer;
-
 
   switch (instruction) {
 
     case '+':
+
       machine->memory[addres] += 1;
-    
+
     break;
 
     case '-':
 
       machine->memory[addres] -= 1;
-
+   
     break;
 
     case '.':
@@ -106,22 +110,54 @@ void sequence_execute(char instruction, VMachine *machine){
       machine->pointer++;
 
     break;
+
+    case '[':
+
+      string prog = "";
+      int brac_size = 1;
+      
+      while (brac_size <= 0) {
+	cout << brac_size;
+	prog+=machine->seq.instructions[pc];
+
+	switch (machine->seq.instructions[pc]) {
+	  case '[':
+	    brac_size++;
+	  break;
+
+	  case ']':
+	    brac_size--;
+	  break;
+	}
+      }
+
+    
+      int last_pointer = machine->memory[machine->pointer];
+
+      while(last_pointer) {
+        brainfuck_execute(prog, machine);
+        last_pointer = machine->memory[machine->pointer];
+      }
+
+    break;
   }
 
 }
 
 
 int main(int argc, const char** argv) {  
+
   ifstream file("prog.bf");
   string code;
 
   vector<int> memory(256,0);
-  
-  VMachine machine = {0, memory};
+ 
+  code = parse_from_stream(file).instructions;   
 
-  code = parse_from_stream(file).instructions;
+  VMachine machine = {0, code, memory};
 
   brainfuck_execute(code, &machine);
   return 0;
+
 }
 
